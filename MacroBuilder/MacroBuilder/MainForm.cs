b1e62fb0a18a8563f0c8b07005da2e4f402a18ce
Zintom <tzsinkoapps@gmail.com>
2021-06-14 13:04:09 +0100

    Create MainForm.cs
    Basic demo of functionality.

---
+++ MacroBuilder/MacroBuilder/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MacroBuilder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var rhk = new RegisterHotKeyWrapper(Handle);
+            rhk.RegisterKey(Keys.Oem8, KeyModifiers.NoRepeat);
+            rhk.RegisterKey(Keys.D, KeyModifiers.NoRepeat);
+            rhk.HotKeyPressed += HotKeyPressed;
+        }
+
+        private void HotKeyPressed(Keys key)
+        {
+            Debug.WriteLine(key.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
